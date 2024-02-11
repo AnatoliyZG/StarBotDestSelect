5da09da42a4c9f05e69bd2f31524252d39827ea1
ZoryaGs <dontneedyou123@mail.ru>
2024-02-11 19:13:48 +0300

    Update 3

--- script.js
+++
@@ -1,9 +0,0 @@
-<script src="https://telegram.org/js/telegram-web-app.js"></script>
-
-var WebApp = window.Telegram.WebApp;
-
-var MainButton = WebApp.MainButton;
-var BackButton = WebApp.BackButton;
-
-MainButton.show();
-BackButton.show();
