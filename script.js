35d47f10285258a574fc6e9bc472f85973478844
ZoryaGs <dontneedyou123@mail.ru>
2024-02-11 19:32:43 +0300

    Update 5

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
 
-let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 
-var MainButton = WebApp.MainButton;
-var BackButton = WebApp.BackButton;
+tg.expand(); //расширяем на все окно  
 
-MainButton.show();
-BackButton.show();
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
 
-MainButton.onClick(function() {
-    WebApp.showAlert("Хорошо, ты нажал на главную кнопку.");
-  });
-  WebApp.onEvent('mainButtonClicked', function() {
-    /* also */
-  });
-  
-  BackButton.onClick(function() {
-    WebApp.showAlert("Нет пути назад!");
-    
-    BackButton.hide();
-  });
-  WebApp.onEvent('backButtonClicked', function() {
-    /* also */
-  });
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isVisible){ //если кнопка показана 
+		tg.MainButton.hide() //скрываем кнопку 
+	}
+  else{ //иначе
+  	tg.MainButton.show() //показываем 
+  }
+});
+
+let btnED = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
+btnED.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isActive){ //если кнопка показана 
+		tg.MainButton.setParams({"color": "#E0FFFF"}); //меняем цвет
+		tg.MainButton.disable() //скрываем кнопку 
+	}
+	else{ //иначе
+		tg.MainButton.setParams({"color": "#143F6B"}); //меняем цвет
+		tg.MainButton.enable() //показываем 
+	}
+});
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send"); 
+	//при клике на основную кнопку отправляем данные в строковом виде
+});
