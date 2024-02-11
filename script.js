6fbd8c813b3e0ba9bc41d4dca3ef80cbb99d2e02
ZoryaGs <dontneedyou123@mail.ru>
2024-02-11 22:17:13 +0300

    Update 7

---
+++ script.js
@@ -0,0 +1,11 @@
+const datalist = document.getElementById('suggestions');
+var options = '';
+fetch('https://anatoliyzg.github.io/StarBotDestSelect/data.txt')
+  .then(response => response.text())
+  .then(data => {
+    const items = data.split('\n');
+    items.forEach(item => {
+        options += '<option value="'+item+'" />';
+    });
+  });
+  datalist.innerHTML=options;
